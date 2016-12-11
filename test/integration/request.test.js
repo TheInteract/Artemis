d5a0b904de073a889051f269df714a25af144d36
ChinCluBi <chinnaporn.s@ku.th>
2016-12-11 22:40:45 +0700

    added /req testing

--- test/integration/request.test.js
+++ test/integration/request.test.js
@@ -7,7 +7,15 @@ chai.use(chaiHttp)
 const request = chai.request.agent(server.listen())
 
 describe('Request to /api/... endpoint', () => {
-    it('GET /api/reg')
+    it('GET /api/reg', (done) => {
+        request.get('/api/reg')
+            .end((err, res) => {
+                expect(err).to.be.null
+                expect(res).to.have.status(200)
+                expect(res.body).to.have.property('token')
+                done()
+            })
+    })
     it('POST /api/event/:type with valid token')
     it('POST /api/event/:type with invalid token')
     it('POST /api/event/:type without token')
