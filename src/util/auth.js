3237e74242eff7eb21492ca15906eca604978fb0
ChinCluBi <chinnaporn.s@ku.th>
2016-12-11 21:58:12 +0700

    added timestamp to authorization header

--- src/util/auth.js
+++ src/util/auth.js
@@ -2,21 +2,27 @@ const config = require('config')
 const crypto = require('crypto')
 const { split } = require('lodash')
 
+function generateToken(timeStamp) {
+    timeStamp = timeStamp || String(new Date().getTime())
+    const hash = crypto.createHmac('sha512', config.get('secret.key')).update(timeStamp)
+    const token = hash.digest('hex')
+    return `${timeStamp}:${token}`
+}
+
 async function auth(ctx, next) {
     const { Authorization } = ctx.headers
-    const token = split(Authorization, ' ', 2)[1]
+    const header = split(Authorization, ' ', 2)[1]
+    if (header === undefined) {
+        ctx.throw('token is undefined', 401)
+    }
+
+    const timeStamp = split(header, ':', 2)[0]
     // TODO: check token from database
-    if (token !== undefined) {
+    if (generateToken(timeStamp) === header) {
         await next()
     } else {
         ctx.throw('token is invalid', 401)
     }
-}
-
-function generateToken() {
-    const hash = crypto.createHmac('sha512', config.get('secret.key')).update(String(new Date().getTime()))
-    const token = hash.digest('hex')
-    return token
 }
 
 module.exports = { auth, generateToken }
