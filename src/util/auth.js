04cd0b8130abdfe5bd7b67e30bdca30d9ba16776
ChinCluBi <chinnaporn.s@ku.th>
2016-12-11 23:39:53 +0700

    separate auth and token

--- src/util/auth.js
+++ src/util/auth.js
@@ -1,14 +1,6 @@
-const config = require('config')
-const crypto = require('crypto')
 const UnauthorizedError = require('../errors/unauthorized')
 const { split } = require('lodash')
-
-function generateToken(timeStamp) {
-    timeStamp = timeStamp || new Date().getTime()
-    const hash = crypto.createHmac('sha512', config.get('secret.key')).update(timeStamp.toString())
-    const token = hash.digest('hex')
-    return `${timeStamp}:${token}`
-}
+const token = require('./token')
 
 async function authorized(authorization) {
     const header = split(authorization, ' ', 2)[1]
@@ -18,10 +10,10 @@ async function authorized(authorization) {
 
     const timeStamp = split(header, ':', 2)[0]
     // TODO: check token from database
-    if (generateToken(timeStamp) !== header) {
+    if (token.generateToken(timeStamp) !== header) {
         throw new UnauthorizedError()
     }
     return true
 }
 
-module.exports = { authorized, generateToken }
+module.exports = { authorized }
