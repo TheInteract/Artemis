e35e27c244d99695ed24af25e06089d1d921dbd9
ChinCluBi <chinnaporn.s@ku.th>
2016-12-12 00:44:41 +0700

    fix bug timestamp type

--- src/util/token.js
+++ src/util/token.js
@@ -3,7 +3,7 @@ const crypto = require('crypto')
 
 function generateToken(timeStamp) {
     timeStamp = timeStamp || new Date().getTime()
-    const hash = crypto.createHmac('sha512', config.get('secret.key')).update(timeStamp.toString())
+    const hash = crypto.createHmac('sha512', config.get('secret.key')).update(String(timeStamp))
     const token = hash.digest('hex')
     return `${timeStamp}:${token}`
 }
