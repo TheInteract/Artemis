a3a8f9a7e773a7851785a2b13897480088df992c
ChinCluBi <chinnaporn.s@ku.th>
2016-12-15 03:39:34 +0700

    fix bug test load server side

--- test/integration/load.test.js
+++ test/integration/load.test.js
@@ -2,20 +2,31 @@ const chai = require('chai')
 const chaiHttp = require('chai-http')
 const server = require('../../src/server')
 const config = require('config')
+const sinon = require('sinon')
+const store = require('../../src/util/store')
 
 const expect = chai.expect
 chai.use(chaiHttp)
 const request = chai.request.agent(server.listen())
 
 describe('Event load', () => {
-    it('POST /api/event/load with authorization', (done) => {
-        request.post('/api/event/load', {})
-            .end((err, res) => {
-                expect(err).to.be.null
-                expect(res).to.have.cookie(config.get('cookie.name'))
-                expect(res).to.have.status(200)
-                done()
-            })
+    describe('server', () => {
+        beforeEach(() => {
+            sinon.stub(store, 'save').returns({})
+        })
+        it('POST /api/event/load with authorization', (done) => {
+            request.post('/api/event/load', {})
+                .end((err, res) => {
+                    expect(err).to.be.null
+                    expect(res).to.have.cookie(config.get('cookie.name'))
+                    expect(res).to.have.status(200)
+                    done()
+                })
+        })
+        it('POST /api/event/load with unauthorization')
+        it('called load event and store to redis')
     })
-    it('POST /api/event/load with unauthorization')
+    describe('client', () => {
+        it('load event happened and post data to server')
+    })
 })
