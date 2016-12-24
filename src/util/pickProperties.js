9d1b0226e49e52390818a17c90f48097efd08507
ChinCluBi <chinnaporn.s@ku.th>
2016-12-24 12:29:17 +0700

    convert properties with function

--- src/util/pickProperties.js
+++ src/util/pickProperties.js
@@ -2,7 +2,14 @@ const transform = require('lodash/transform')
 const get = require('lodash/get')
 
 function pickProperties(input, properties) {
-    return transform(properties, (result, value, key) => { result[key] = get(input, value) }, {})
+    return transform(properties, (result, value, key) => {
+        const v = get(input, value.target || value)
+        if (value.fn && typeof value.fn === 'function') {
+            result[key] = value.fn(v)
+        } else {
+            result[key] = v
+        }
+    }, {})
 }
 
 module.exports = pickProperties
