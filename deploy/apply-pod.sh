5eb6e41633ca7a67a9802683e143a8e8b181495d
ChinCluBi <chinnaporn.s@ku.th>
2017-01-02 14:56:24 +0700

    add collector service

--- deploy/apply-pod.sh
+++ deploy/apply-pod.sh
@@ -7,3 +7,6 @@ kubectl --namespace $ENV apply -f ./components/redis-master.yml
 
 kubectl --namespace $ENV replace -f ./components/redis-slave.yml
 kubectl --namespace $ENV apply -f ./components/redis-slave.yml
+
+kubectl --namespace $ENV replace -f ./components/collector.yml
+kubectl --namespace $ENV apply -f ./components/collector.yml
--- deploy/create-pod.sh
+++ deploy/create-pod.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+kubectl create -f ./kube-system/traefik.yml
+
 ENV=production
 
 kubectl create namespace $ENV
 
+kubectl create --namespace $ENV -f ./components/collector.yml
 kubectl create --namespace $ENV -f ./components/redis-master.yml
 kubectl create --namespace $ENV -f ./components/redis-slave.yml
