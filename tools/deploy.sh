af0e2153354adee8b8ea5dde5f3fd9992f33b1f6
ChinCluBi <chinnaporn.s@ku.th>
2017-01-14 01:06:24 +0700

    remove shell script

--- tools/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!bin/bash
-
-ENV=production
-
-kubectl --namespace $ENV patch deployment collector-server \
-    -p'{"spec":{"template":{"spec":{"containers":[{"name":"collector-server","image":"chinclubi/collector:latest"}]}}}}'
