7024ea32052968188adca8c9c8b74b7872293551
Chonnipa Kittisiriprasert <Nagisama.j@gmail.com>
2017-02-08 14:57:50 +0700

    receive enabled features instead of disabled features

--- client/events/load/index.js
+++ client/events/load/index.js
@@ -5,6 +5,16 @@ function handleLoadEvent (e) {
   const data = pickProperties(e, PROPERTIES)
   data.uid = this.uid
   this.fetch.post('/event/load', data)
+    .then(response => {
+      const enabledFeatures = [
+        '[interact-feature="card-1"]:not([interact-feature-type="b"])',
+        '[interact-feature="card-2"]:not([interact-feature-type="a"])'
+      ]
+
+      const selectors = enabledFeatures.join(', ')
+      const elements = document.querySelectorAll(selectors)
+      elements.forEach(element => { element.remove() })
+    })
 }
 
 module.exports = handleLoadEvent
