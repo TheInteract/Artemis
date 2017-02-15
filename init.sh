fe185135f0b2142dcf6f16c2b1385b73a02f5414
Thanyaboon Tovorapan <whatineat@gmail.com>
2017-02-15 16:03:03 +0700

    Edited readme and init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 #!bin/bash
 docker exec -it interact-mongo mongo
 use interact
+db.createCollection("customer")
+db.createCollection("user")
 db.customer.insert({"customerCode":"IC9-55938-5","hostname":"localhost","features":[{"name":"card-1","types":[{"typeName":"A","percent":0},{"typename":"B","percent":0}]},{"name":"card-2","types":[{"typeName":"A","percent":0},{"typename":"B","percent":0}]}]})
 exit
