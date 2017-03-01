c260bab94953e0b3a9a3b72b2f7e5088d8d563ed
Chonnipa Kittisiriprasert <Nagisama.j@gmail.com>
2017-03-01 20:04:01 +0700

    :white_check_mark: fix get product by public and private key

--- server/src/products/Products.spec.js
+++ server/src/products/Products.spec.js
@@ -8,29 +8,54 @@ import sinon from 'sinon'
 const assert = chai.assert
 
 describe('Products', () => {
+  before(() => {
+    sinon.stub(Collections, 'findItem')
+    Collections.findItem.returns('items')
+  })
+
+  after(() => {
+    Collections.findItem.restore()
+  })
+
   describe('getProductByPrivateKey', async () => {
-    before(() => {
-      sinon.stub(Collections, 'findItem')
-      Collections.findItem.returns('items')
-    })
-
-    after(() => {
-      Collections.findItem.restore()
-    })
-
-    const fakeApiKey = 'fakeApiKey'
+    const fakePrivateApiKey = 'fakePrivateApiKey'
     const fakeIp = 'fakeIp'
 
     it('should return items from Collections.findItem', async () => {
-      const product = await Products.getProductByPrivateKey(fakeApiKey, fakeIp)
+      const product = await Products.getProductByPrivateKey(
+        fakePrivateApiKey,
+        fakeIp
+      )
       assert.equal(product, 'items')
     })
 
     it('should called findItem with correct arguments', () => {
       assert(Collections.findItem.calledWith(
         config.mongo.collections.names.product, {
-          API_KEY_PRIVATE: fakeApiKey,
+          API_KEY_PRIVATE: fakePrivateApiKey,
           ip: fakeIp
+        }
+      ), 'invalid arguments')
+    })
+  })
+
+  describe('getProductByPublicKey', async () => {
+    const fakePublicApiKey = 'fakePublicApiKey'
+    const fakeDomainName = 'fakeDomainName'
+
+    it('should return items from Collections.findItem', async () => {
+      const product = await Products.getProductByPublicKey(
+        fakePublicApiKey,
+        fakeDomainName
+      )
+      assert.equal(product, 'items')
+    })
+
+    it('should called findItem with correct arguments', () => {
+      assert(Collections.findItem.calledWith(
+        config.mongo.collections.names.product, {
+          API_KEY_PUBLIC: fakePublicApiKey,
+          domainName: fakeDomainName
         }
       ), 'invalid arguments')
     })
