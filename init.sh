ebdfa73f5116f428ba3ca70518aed7b2731ec110
Chonnipa Kittisiriprasert <Nagisama.j@gmail.com>
2017-03-07 18:17:01 +0700

    :art: remove insert user

--- init.sh
+++ init.sh
@@ -5,5 +5,4 @@ db.createCollection("user")
 db.createCollection("feature")
 db.createCollection("version")
 db.product.insert({ "API_KEY_PRIVATE": "IC9-55938-5", "API_KEY_PUBLIC": "12345", "ip": "158.108.136.125", "domainName": "localhost:9999" })
-db.user.insert({ "userIdentity": "fakeHashedUserId-1" })
 exit
