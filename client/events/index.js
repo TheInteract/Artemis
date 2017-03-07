78f7f0f6f7464f646fd1b84746427908c7936eeb
Thanyaboon Tovorapan <whatineat@gmail.com>
2017-03-07 15:43:59 +0700

    Fixed API call overrider

--- client/events/index.js
+++ client/events/index.js
@@ -6,6 +6,7 @@ const mousemoveProperties = require('./mousemove/properties')
 const resizeProperties = require('./resize/properties')
 const scrollProperties = require('./scroll/properties')
 const pickProperties = require('../util/pickProperties')
+const url = require('url')
 
 const propertiesObject = {
   APICall: APIProperties,
@@ -22,21 +23,40 @@ let prevTime = {
   resize: new Date(0)
 }
 
+function callFetch (fetch, type, data) {
+  fetch.post('/event/on' + type, data)
+}
+
+function requestIsNotInDelay (type) {
+  const now = new Date()
+  if (now - prevTime[type] > 500) {
+    prevTime[type] = new Date()
+    return true
+  }
+  return false
+}
+
+function isCallToProductEndPoint (targetHostname) {
+  const productHostname = window.location.hostname
+  const artemisHostname = url.parse(process.env.COLLECTOR_BASE || 'http://localhost:3000/').hostname
+
+  if (targetHostname === productHostname && targetHostname !== artemisHostname) {
+    return true
+  }
+  return false
+}
+
 function handleEvent (type, event) {
   const data = pickProperties(event, propertiesObject[type])
-  if (type === 'mousemove' || type === 'resize') {
-    const now = new Date()
-    if (now - prevTime[type] > 500) {
-      prevTime[type] = new Date()
-    } else {
-      return
-    }
-  } else if (type === 'APICall') {
-    if (data.url.includes(process.env.COLLECTOR_BASE || 'http://localhost:3000/')) return
-  }
-
+  const fetch = this.fetch
   data.API_KEY_PUBLIC = this.API_KEY_PUBLIC
-  this.fetch.post('/event/on' + type, data)
+
+  if (!(
+    ((type === 'mousemove' || type === 'resize') && !requestIsNotInDelay(type)) ||
+    (type === 'APICall' && !isCallToProductEndPoint(data.url.hostname))
+  )) {
+    callFetch(fetch, type, data)
+  }
 }
 
 module.exports = handleEvent
