c61017ef3f4262eeb8465b9f49b615c4f63042e9
Chonnipa Kittisiriprasert <Nagisama.j@gmail.com>
2017-03-07 18:18:43 +0700

    :art: remove console.log

--- server/src/features/Features.js
+++ server/src/features/Features.js
@@ -4,7 +4,6 @@ import Mongodb from 'mongodb'
 import config from 'config'
 
 export const getFeaturesByProduct = async (productId) => {
-  console.log('productId', productId)
   return await Collections.findItems(config.mongo.collections.names.feature, {
     productId: Mongodb.ObjectId(productId)
   })
