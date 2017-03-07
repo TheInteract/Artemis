8b8653548215c7f74a955e8d97165dca4906b76d
pNagi <nagisama.j@gmail.com>
2017-03-07 20:57:03 +0700

    :bug: fix FeatureListItem.create to return featureName

--- server/src/feature-list/FeatureListItem.js
+++ server/src/feature-list/FeatureListItem.js
@@ -1,6 +1,6 @@
 import * as Features from '../features/Features'
 
 export const create = async version => ({
-  featureName: await Features.getFeature(version.featureId),
+  featureName: await Features.getFeature(version.featureId).name,
   version: version.name
 })
