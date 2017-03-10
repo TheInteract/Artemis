a70e3bc7ca768f120709a7f5433881c8d1d21df5
Thanyaboon Tovorapan <whatineat@gmail.com>
2017-03-10 15:45:40 +0700

    Removed unused events in server

--- server/src/events-deprecated/Resize.js
+++
@@ -1,8 +0,0 @@
-import omit from 'lodash/omit'
-import store from '../util/store'
-
-export default async (cookie, body) => {
-  const { API_KEY } = body
-  const rest = omit(body, [ 'API_KEY' ])
-  await store.save(API_KEY, cookie, rest, 'resize')
-}
--- server/src/events-deprecated/index.js
+++
@@ -1,6 +0,0 @@
-export onload from './Load'
-export onclick from './Click'
-export onkeydown from './KeyDown'
-export onscroll from './Scroll'
-export onresize from './Resize'
-export onmousemove from './MouseMove'
