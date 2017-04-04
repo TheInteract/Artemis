1b5e2ace338e769e9e13235f6358f53beb883c17
ChinCluBi <chinnaporn.s@ku.th>
2017-04-04 14:25:52 +0700

    fix artemis base on production

--- client/webpack.prod.config.js
+++ client/webpack.prod.config.js
@@ -27,7 +27,7 @@ module.exports = {
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: JSON.stringify('production'),
-        COLLECTOR_BASE: JSON.stringify('http://localhost:3000/'),
+        COLLECTOR_BASE: JSON.stringify('http://35.186.235.13/'),
       },
     }),
     new webpack.optimize.UglifyJsPlugin({
