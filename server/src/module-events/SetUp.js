ebd70f6f2b9d989b1885064612f928801884097a
ChinCluBi <chinnaporn.s@ku.th>
2017-04-04 13:23:56 +0700

    load server host from config

--- server/src/module-events/SetUp.js
+++ server/src/module-events/SetUp.js
@@ -3,6 +3,8 @@ import * as FeatureLists from '../feature-list/FeatureLists'
 import * as Products from '../products/Products'
 import * as Users from '../users/Users'
 
+import config from 'config'
+
 const SetUp = async ctx => {
   const { body, ip } = ctx.request
   const { API_KEY_PRIVATE } = body
@@ -21,11 +23,10 @@ const SetUp = async ctx => {
     initCode: getInitCode(API_KEY_PUBLIC),
   }
   ctx.status = 200
-  console.log(ctx.body)
 }
 
 const getInitCode = (API_KEY_PUBLIC) => (
-  `!function(e,t,n,c,a){e.INIT=function(t){e.i=t},c=t.createElement(n),a=t.getElementsByTagName(n)[0],c.async=!0,c.src="http://10.2.61.137:3000/analytics.js",a.parentNode.insertBefore(c,a)}(window,document,"script"),INIT("${API_KEY_PUBLIC}");`
+  `!function(e,t,n,c,a){e.INIT=function(t){e.i=t},c=t.createElement(n),a=t.getElementsByTagName(n)[0],c.async=!0,c.src="http://${config.get('server.host')}:${config.get('server.port')}/analytics.js",a.parentNode.insertBefore(c,a)}(window,document,"script"),INIT("${API_KEY_PUBLIC}");`
 )
 
 export default SetUp
