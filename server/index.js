5be507c40d4fabfad0ae24efba8c15a85e5e428c
ChinCluBi <chinnaporn.s@ku.th>
2017-04-11 12:29:16 +0700

    :art: remove useless code

--- server/index.js
+++ server/index.js
@@ -1,14 +1,3 @@
-const url = require('url')
-const config = require('config')
-
-const basePath = config.get('base')
-
 require('babel-register')
-// Client Side is not support
-global.rootRequire = (name) => {
-  const dir = url.join(__dirname, basePath, name)
-  return require(dir)
-}
-
 require('./util/log.js')
 require('./server.js')
