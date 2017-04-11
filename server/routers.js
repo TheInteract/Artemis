1e69848877391ae0df56e3c8eac2e24e4f07c67e
ChinCluBi <chinnaporn.s@ku.th>
2017-04-11 12:32:12 +0700

    :bug: fixed prefix path on production

--- server/routers.js
+++ server/routers.js
@@ -1,11 +1,12 @@
+import * as HandleEvent from './src/module-events/HandleEvent'
 import * as Product from './src/products/Product'
 
 import Router from 'koa-router'
 import SetUp from './src/module-events/SetUp'
-import * as HandleEvent from './src/module-events/HandleEvent'
+import config from 'config'
 import endpoints from './util/endpoints'
 
-const router = new Router({ prefix: '/api' })
+const router = new Router({ prefix: config.prefix })
 
 router.get('/healthz', (ctx) => { ctx.status = 200 })
 router.post(endpoints.INIT_EVENT, Product.authorized, SetUp)
